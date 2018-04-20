8f38229d81e60690123f7fe4c5a4d2b4c4abfea3
mobydimk <mobydimk@gmail.com>
2018-04-20 22:00:27 +0400

    svn lib update

--- server/libs/svn.js
+++ server/libs/svn.js
@@ -2,9 +2,14 @@ var child_process_exec = require('child_process').exec
 var config = require('../config')
 var url = require('url')
 
-function authParams(credentials) {
+function auth(credentials) {
     return ' --username "' + credentials.username.replace(/"/g, '\\"') + '" ' +
         '--password "' + credentials.password.replace(/"/g, '"') + '"'
+}
+
+
+function cd() {
+    return 'cd "' + config.path + '" &&  ';
 }
 
 
@@ -24,7 +29,7 @@ function exec(cmd, callback) {
 module.exports = {
 
     list: (credentials, callback) => {
-        var cmd = 'svn list ' + config.svn_repository + authParams(credentials)
+        var cmd = 'svn list ' + config.svn_repository + auth(credentials)
         exec(cmd, (err, stdout) => {
             if(err) return callback(err)
             var folders = stdout.replace(/\//g, '').split(/\r?\n/).filter(item => item != '')
@@ -36,7 +41,7 @@ module.exports = {
     checkout: (credentials, folders, callback) => {
         if(!folders.length) return callback()
         folders = folders.map(folder => url.resolve(config.svn_repository, folder))
-        var cmd = 'cd ' + config.path + ' && svn co ' + folders.join(' ') + ' ' + authParams(credentials)
+        var cmd = cd() + 'svn co ' + folders.join(' ') + ' ' + auth(credentials)
         exec(cmd, callback)
     },
 
@@ -46,38 +51,38 @@ module.exports = {
         if(!Array.isArray(folders)) {
             folders = [folders]
         }
-        var cmd = 'cd ' + config.path + ' && svn update ' + folders.join(' ') + ' ' + authParams(credentials)
+        var cmd = cd() + 'svn update ' + folders.join(' ') + ' ' + auth(credentials)
         exec(cmd, callback)
     },
 
 
     add: (credentials, path, callback) => {
-        var cmd = 'cd ' + config.path + ' && svn add ' + path + ' --force'
+        var cmd = cd() + 'svn add ' + path + ' --force'
         exec(cmd, callback)
     },
 
 
     commit: (credentials, path, callback) => {
-        var cmd = 'cd ' + config.path + ' && svn commit ' + path + ' ' + authParams(credentials) + ' --message "Task editor"'
+        var cmd = cd() + 'svn commit ' + path + ' ' + auth(credentials) + ' --message "Task editor"'
         exec(cmd, callback)
     },
 
 
     delete: (credentials, path, callback) => {
-        var cmd = 'cd ' + config.path + ' && svn rm ' + path
+        var cmd = cd() + 'svn rm ' + path
         exec(cmd, callback)
     },
 
 
     revert: (credentials, path, callback) => {
-        var cmd = 'cd ' + config.path + ' && svn revert ' + path
+        var cmd = cd() + 'svn revert ' + path
         exec(cmd, callback)
     },
 
 
     cleanup: (credentials, path, callback) => {
-        var cmd = 'cd ' + config.path +
-            ' && svn status ' + path +
+        var cmd = cd() +
+            'svn status ' + path +
             ' --no-ignore | grep \'^[I?]\' | cut -c 9- | while IFS= read -r f; do rm -rf "$f"; done';
         exec(cmd, callback)
     }
